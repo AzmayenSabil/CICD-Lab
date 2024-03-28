5fd6d88d1ad26653016f43909515bf1bfa54f11a
Azmayen Fayek Sabil <azmayenfayek@iut-dhaka.edu>
2024-03-28 12:09:35 +0600

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Welcome Azure Application')
+    res.send('Welcome Azure Application For software verification')
 })
 
 app.listen(process.env.PORT, () => {
